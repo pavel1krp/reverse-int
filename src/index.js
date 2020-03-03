ac744050be145a75402808ce341168fc6a01b22d
pavel1krp <60657881+pavel1krp@users.noreply.github.com>
2020-03-03 23:30:15 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let r = 0
+    if (n > 1) {
+        do {
+            r = r * 10 + n % 10;
+            n = Math.floor(n / 10);
+        }
+        while (n > 0);
+    }
+    else {
+        n = n * (-1)
+        do {
+
+            r = r * 10 + n % 10;
+            n = Math.floor(n / 10);
+        }
+
+        while (n > 0);
+    }
+    return r;
 }
+
